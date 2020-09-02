5eea3d629248686bd06c4c16ad21d42bbe63d354
subhayuroy <subhayu.roy2001@gmail.com>
2020-09-02 20:18:34 +0530

    Scrape the website

--- news/views.py
+++ news/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+import requests
+from bs4 import BeautifulSoup as BSoup
+from news.models import HeadLine
+
+def scrape(request):
+    session = requests.Session()
+    session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+    url = "https://www.theonion.com/"
+
+    content = session.get(url, verify=False).content
+    soup = BSoup(content, "html.parser")
+    News = soup.find_all('div', {"class": "curation-module__item"})
+    return redirect("../")
